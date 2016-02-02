c4273d3f3a6ce3b9489c0bb95ee41a24d4a1124d
Your Name <you@example.com>
2016-02-02 19:44:03 +0000

    minor breakthrough

---
+++ client/containers/blog_titles.js
@@ -0,0 +1,25 @@
+import {composeWithTracker} from 'react-komposer'
+import BlogTitles from '../components/3.blog_titles.jsx'
+
+function composer(props, onData) {
+  const handle = Meteor.subscribe('posts')
+  if(handle.ready()) {
+    const posts = Posts.find({}, {sort: {_id: 1}}).fetch()
+    onData(null, {posts})
+  }
+}
+
+BlogTitlesContainer = GraphQL.bindData((props, onData) => {
+  return BlogSchema.watchQuery(BlogTitlesContainer.query, onData)
+})(BlogTitles);
+
+BlogTitlesContainer.query = `
+  {
+    posts {
+      ...${BlogTitles.fragment}
+    }
+  }
+`;
+
+export default composeWithTracker(composer)(BlogTitles)
+
